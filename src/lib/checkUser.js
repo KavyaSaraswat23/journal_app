14b0662362ecc43365a0d58748f296b289a9d44c
Kavya Saraswat <kavyasaraswat4@gmail.com>
2025-12-02 02:12:42 +0530

    write the logic for checking the current user Exist or not

--- src/lib/checkUser.js
+++ src/lib/checkUser.js
@@ -1,39 +1,39 @@
 
-import {currentUser} from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
 import { prisma } from "./prisma";
 
-export async function checkUser(){
-    const user = await currentUser();
+export async function checkUser() {
+  const user = await currentUser();
 
-    if (!user){
-        return null //checking for now
+  if (!user) {
+    return null //checking for now
+  }
+
+  try {
+    const isUser = await prisma.user.findUnique({
+      where: {
+        clerkUserId: user.id
+      }
+    });
+
+    if (isUser) {
+      return isUser
     }
 
-    try {
-        const isUser = await prisma.user.findUnique({
-            where: {
-                clerkUserId: user.id
-            }
-        });
 
-        if (isUser){
-            return isUser
-        }
+    const newUser = await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        name: user.fullName,
+        imageUrl: user.imageUrl,
 
-        
-        const newUser = await prisma.user.create({
-            data: {
-                clerkUserId: user.id,
-                email: user.emailAddresses[0].emailAddress,
-                name: user.fullName,
-                imageUrl: user.imageUrl,
+      }
+    });
 
-            }
-        });
-
-        return newUser
-    } catch (error) {
-        console.log(error.message)
-    }
-}   
+    return newUser
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
