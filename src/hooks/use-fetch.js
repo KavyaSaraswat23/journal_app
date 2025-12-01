48c9a417e2f14d6b1791a4b9253abf86f0f17d56
Kavya Saraswat <kavyasaraswat4@gmail.com>
2025-12-02 02:36:24 +0530

    created the custom hook for getting data from all the bussiness logix that we will create

--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -1,28 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { toast } from 'sonner';
 
 const useFetch = (cb) => {
-    const [data, setData] = useState(undefined);
-    const [loading, setLoading] = useState(null);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
 
-    async function fn(...args){
-        setLoading(true);
-        setError(null);
+  async function fn(...args) {
+    setLoading(true);
+    setError(null);
 
-        try {
-            const response = await cb(...args);
-            setData(response);
-            setError(null);
-        } catch (err) {
-            setError(err);
-            toast.error(err.message);
-        }finally {
-            setLoading(false);
-        }
+    try {
+      const response = await cb(...args);
+      setData(response);
+      setError(null);
+    } catch (err) {
+      setError(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
     }
+  }
 
-  return {data, loading, error, fn, setData}
+  return { data, loading, error, fn, setData }
 }
 
 export default useFetch
