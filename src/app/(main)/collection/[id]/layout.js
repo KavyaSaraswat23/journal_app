494d202f2adcdf9f54507059c8c0579dc76f178f
Kavya Saraswat <kavyasaraswat4@gmail.com>
2025-12-02 10:25:47 +0530

    refactor CollectionLayout component for improved code readability and formatting

--- src/app/(main)/collection/[id]/layout.js
+++ src/app/(main)/collection/[id]/layout.js
@@ -1,18 +1,18 @@
 import { ArrowBigLeftIcon, ArrowLeftIcon, MoveLeft } from 'lucide-react';
 import Link from 'next/link';
 import React, { Suspense } from 'react'
-import {BarLoader} from "react-spinners";
+import { BarLoader } from "react-spinners";
 const CollectionLayout = ({ children, params }) => {
   return (
     <div className=' mx-auto px-4 py-8 container'>
-        <div className=''>
-            <Link href='/dashboard' className='text-sm text-orange-600 hover:text-orange-700 cursor-pointer'>
-                <ArrowLeftIcon /> Back to dashboard
-            </Link>
-        </div>
-        <Suspense fallback={<BarLoader color='orange' width={"100%"} />}> 
-          {children}
-        </Suspense>
+      <div className=''>
+        <Link href='/dashboard' className='text-sm text-orange-600 hover:text-orange-700 cursor-pointer'>
+          <ArrowLeftIcon /> Back to dashboard
+        </Link>
+      </div>
+      <Suspense fallback={<BarLoader color='orange' width={"100%"} />}>
+        {children}
+      </Suspense>
     </div>
   )
 }
