346e2acac57aaac4ff72d0447ecbe2d57b81f3ea
Kavya Saraswat <kavyasaraswat4@gmail.com>
2025-12-02 10:11:17 +0530

    made the navbar

--- src/components/usermenu.jsx
+++ src/components/usermenu.jsx
@@ -6,21 +6,22 @@ import React from 'react'
 const UserMenu = () => {
   return (
     <div>
-        {/* Can change my UI here  Must check*/}
-      <UserButton 
-      appearance={{
-        elements: {
+      {/* Can change my UI here  Must check*/}
+      <UserButton
+        appearance={{
+          elements: {
             avatarBox: 'w-19 h-19'
-        }}
-      }>
+          }
+        }
+        }>
         <UserButton.MenuItems>
 
-            <UserButton.Link 
-                label="Dashboard"
-                labelIcon={<ChartNoAxesGantt size={15} />}
-                href='/dashboard'
-            />
-            <UserButton.Action label="manageAccount" /> 
+          <UserButton.Link
+            label="Dashboard"
+            labelIcon={<ChartNoAxesGantt size={15} />}
+            href='/dashboard'
+          />
+          <UserButton.Action label="manageAccount" />
         </UserButton.MenuItems>
       </UserButton>
     </div>
